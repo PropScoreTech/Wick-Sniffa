10b085dce3306b1af470bee51cc9c33d9aae2c66
Kyle - PropScore <299236595+PropScoreTech@users.noreply.github.com>
2026-07-21 18:24:57 -0400

    Delete sniff-counter-app directory

--- sniff-counter-app/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('sniffAPI', {
-  loadCount: () => ipcRenderer.invoke('load-count'),
-  saveCount: (n) => ipcRenderer.invoke('save-count', n),
-  loadBg: () => ipcRenderer.invoke('load-bg'),
-  saveBg: (mode) => ipcRenderer.invoke('save-bg', mode),
-  closeApp: () => ipcRenderer.send('close-app'),
-});
